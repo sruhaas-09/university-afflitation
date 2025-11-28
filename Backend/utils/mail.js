b4252000f630151eaf165e36c6c44ac152750527
sruhaas-09 <sruhaaskarthia@gmail.com>
2025-11-28 21:15:19 +0530

    Update email transport configuration
    Convert EMAIL_PORT to a number and add TLS options.

--- Backend/utils/mail.js
+++ Backend/utils/mail.js
@@ -4,11 +4,14 @@ const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      port: Number(process.env.EMAIL_PORT),
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+      },
+        tls: {
+        rejectUnauthorized: false,
       },
     });
 
